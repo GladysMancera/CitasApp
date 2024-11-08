7eae2fbc18ef1d470e438de3e1da1706f215f2c0
Gladys Mancera <g.gladysmancera@gmail.com>
2024-11-08 11:10:34 -0600

    Diseño de la App

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -9,10 +9,24 @@ import { Router } from '@angular/router';
 export class ProfilePage implements OnInit {
   // Definición de la información del usuario
   user = {
-    nombre: 'Gladys Dayanni',
+    nombre: 'Gladys Dayanni Mancera Rio de la losa',
     descripcion: 'Estudiante',
-    // Agrega otros datos del usuario si es necesario
+    fotoUrl: 'assets/perfil.jpg', // Cambia esto con la URL de la foto del perfil
   };
+
+  // Definición de las citas registradas
+  citas = [
+    {
+      titulo: 'Consulta Médica General',
+      fecha: new Date('2024-11-10'),
+      descripcion: 'Revisión médica para chequeo general de salud.',
+    },
+    {
+      titulo: 'Ginecología',
+      fecha: new Date('2024-11-15'),
+      descripcion: 'Chequeo general.',
+    },
+  ];
 
   // Inyectamos Router en el constructor para poder navegar entre páginas
   constructor(private router: Router) {}
@@ -21,8 +35,8 @@ export class ProfilePage implements OnInit {
     // Cualquier lógica de inicialización puede ir aquí
   }
 
+  // Método para navegar a la página de citas médicas
   verCitas() {
-    // Navegar a la página de citas médicas
-    this.router.navigate(['/citas']);
+    this.router.navigate(['/appoiments']);
   }
 }
